26f52a155b5d63909d7593decc64920e311506ec
wdwf <w_dwf@hotmail.com>
2023-07-30 18:08:22 -0300

    :wrench: chore: add jest setup and config

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,10 @@
 import * as express from "express"
+import routes from "./routes"
 
 const app = express()
 const port = 3333
+
+app.use(routes)
 
 app.listen(port, () => {
   console.log(`Api conectada na porta ${port}`)
