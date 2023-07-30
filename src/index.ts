9de6567262517f8167a0b4542b56e61089093193
wdwf <w_dwf@hotmail.com>
2023-07-30 16:38:35 -0300

    :wrench: chore: add eslint and prettier setup

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,8 @@
-console.log("hello!!");
+import * as express from "express"
+
+const app = express()
+const port = 3333
+
+app.listen(port, () => {
+  console.log(`Api conectada na porta ${port}`)
+})
